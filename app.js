e5d63454c40cadb17e72ec18875525b34ad8f7bb
Otto Rotor <ambermoorexc@gmail.com>
2020-09-21 18:02:01 -0500

    Get requests and fixed js path file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Need to capture the city search
+// Need to know Austin city_id = 278
 // grab the city id
 // that chains to another url request (establishments?)
 // returns two results
@@ -7,23 +7,83 @@
 // no longer pulls request after 10 decisions
 // once the 10 decision has been hit-last decision will be shown
 
-// url for searching your city
-// `https://developers.zomato.com/api/v2.1/cities?q=${city}`
-
-// url for returning establishments
-// `https://developers.zomato.com/api/v2.1/establishments?city_id=${cityId}`
 
 
-async function getList(cityId) {
-  const url = 'https://developers.zomato.com/api/v2.1/establishments?city_id=278&user-key=d2595947349326ccfb5c1ce5e27e77b2'
+function start() {
+  axios({
+    method: 'get',
+    url: 'https://developers.zomato.com/api/v2.1/search?entity_id=278&entity_type=city&count=2&collection_id=1',
+    headers: {
+      'user-key': 'd2595947349326ccfb5c1ce5e27e77b2'
+    }
+  })
+    .then((res) => {
+      console.log(res.data.restaurants)
+      const response = res.data.restaurants
+    }).catch((err) => {
+      console.error(err)
+    })
+}
+
+
+
+
+
+
+
+
+function getData() {
+  axios({
+    method: 'get',
+    url: 'https://developers.zomato.com/api/v2.1/search?entity_id=278&entity_type=city&count=20&collection_id=1',
+    headers: {
+      'user-key': 'd2595947349326ccfb5c1ce5e27e77b2'
+    }
+  })
+    .then((res) => {
+      console.log(res.data.restaurants)
+      const response = res.data.restaurants
+      response.forEach((estab) => {
+        const testingDiv = document.querySelector('.testing')
+        const restaurantDiv = document.createElement('div')
+        testingDiv.appendChild(restaurantDiv)
+
+        const name = document.createElement('h3')
+        const restName = estab.restaurant.name
+        name.textContent = `${restName}`
+        restaurantDiv.appendChild(name)
+      })
+    }).catch((err) => {
+      console.error(err)
+    })
+}
+// getData()
+
+async function getData2() {
   try {
-    const response = await axios.get(url)
-    console.log(response)
-  } catch (error) {
-    console.log(`Error: ${error}`)
+    const res = await axios({
+      method: 'get',
+      url: 'https://developers.zomato.com/api/v2.1/search?entity_id=278&entity_type=city&count=20&collection_id=1',
+      headers: {
+        'user-key': 'd2595947349326ccfb5c1ce5e27e77b2'
+      }
+    });
+
+    console.log(res.data.restaurants)
+    const response = res.data.restaurants
+    response.forEach((estab) => {
+      const testingDiv = document.querySelector('.testing')
+      const restaurantDiv = document.createElement('div')
+      testingDiv.appendChild(restaurantDiv)
+
+      const name = document.createElement('h3')
+      const restName = estab.restaurant.name
+      name.textContent = `${restName}`
+      restaurantDiv.appendChild(name)
+    })
+  } catch (err) {
+    console.error(err)
   }
 }
-getList(278)
-
-
+getData2()
 
