4003c60163669731870b855ff68745273a6d4869
Otto Rotor <ambermoorexc@gmail.com>
2020-09-22 10:40:01 -0500

    Appending items

--- app.js
+++ app.js
@@ -7,59 +7,9 @@
 // no longer pulls request after 10 decisions
 // once the 10 decision has been hit-last decision will be shown
 
-
-
-function start() {
-  axios({
-    method: 'get',
-    url: 'https://developers.zomato.com/api/v2.1/search?entity_id=278&entity_type=city&count=2&collection_id=1',
-    headers: {
-      'user-key': 'd2595947349326ccfb5c1ce5e27e77b2'
-    }
-  })
-    .then((res) => {
-      console.log(res.data.restaurants)
-      const response = res.data.restaurants
-    }).catch((err) => {
-      console.error(err)
-    })
-}
-
-
-
-
-
-
-
-
-function getData() {
-  axios({
-    method: 'get',
-    url: 'https://developers.zomato.com/api/v2.1/search?entity_id=278&entity_type=city&count=20&collection_id=1',
-    headers: {
-      'user-key': 'd2595947349326ccfb5c1ce5e27e77b2'
-    }
-  })
-    .then((res) => {
-      console.log(res.data.restaurants)
-      const response = res.data.restaurants
-      response.forEach((estab) => {
-        const testingDiv = document.querySelector('.testing')
-        const restaurantDiv = document.createElement('div')
-        testingDiv.appendChild(restaurantDiv)
-
-        const name = document.createElement('h3')
-        const restName = estab.restaurant.name
-        name.textContent = `${restName}`
-        restaurantDiv.appendChild(name)
-      })
-    }).catch((err) => {
-      console.error(err)
-    })
-}
-// getData()
-
-async function getData2() {
+// FUNCTION THAT WILL GET RESTAURANT OBJECTS AND APPEND TO DOM
+// ARGUMENTS: WHICH DIV CONTAINER AND WHICH DATA ARRAY TO USE
+async function getData(div, div2, arr) {
   try {
     const res = await axios({
       method: 'get',
@@ -69,21 +19,51 @@ async function getData2() {
       }
     });
 
-    console.log(res.data.restaurants)
+    // VARIABLES
     const response = res.data.restaurants
-    response.forEach((estab) => {
-      const testingDiv = document.querySelector('.testing')
-      const restaurantDiv = document.createElement('div')
-      testingDiv.appendChild(restaurantDiv)
+    // console.log(response)
+    let first10 = response.slice(0, 10)
+    let randomFirst10 = first10[Math.floor(Math.random() * first10.length)]
+    let last10 = response.slice(10)
+    let randomLast10 = last10[Math.floor(Math.random() * last10.length)]
+    const leftBox = document.querySelector('#left')
+    const rightBox = document.querySelector('#right')
+    const leftListBox = document.querySelector('#leftListBox')
+    const rightListBox = document.querySelector('#rightListBox')
 
-      const name = document.createElement('h3')
-      const restName = estab.restaurant.name
-      name.textContent = `${restName}`
-      restaurantDiv.appendChild(name)
-    })
+    // NAME OF RESTAURANT
+    let name = document.createElement('h3')
+    name.textContent = `${arr.restaurant.name}`
+    div.appendChild(name)
+    // IMG OF RESTAURANT
+    let image = document.createElement('img')
+    image.src = arr.restaurant.featured_image
+    div.appendChild(image)
+    // CUISINE TYPE
+    let cuisine = document.createElement('li')
+    cuisine.innerHTML = `Cuisine : ${arr.restaurant.cuisines}`
+    div2.appendChild(cuisine)
+    // AVERAGE COST OF 2
+    let cost = document.createElement('li')
+    cost.innerHTML = `Average cost for 2: ${arr.restaurant.average_cost_for_two}`
+    div2.appendChild(cost)
+    // HOURS OF OPERATION
+    let timings = document.createElement('li')
+    timings.innerHTML = `Hours : ${arr.restaurant.timings}`
+    div2.appendChild(timings)
+
+
+
   } catch (err) {
     console.error(err)
   }
 }
-getData2()
+
+// AFTER EVENT LISTENER, THIS WOULD BE FUNCTION FOR 'THIS' BUTTON
+getData(rightBox, rightListBox, randomLast10)
+
+// AFTER EVENT LISTENER, THIS WOULD BE FUNCTION FOR 'THAT' BUTTON
+getData(leftBox, leftListBox, randomFirst10)
+
+
 
