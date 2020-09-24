ad72a60fd3c60cde2b1bd293877316f842ec2b52
Otto Rotor <ambermoorexc@gmail.com>
2020-09-24 18:11:53 -0500

    Everything is working with new API

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function grabPortion(data, side) {
   let leftRestaurant = data.shift()
   // console.log(leftRestaurant)
   let rightRestaurant = data.pop()
-  if (leftRestaurant && rightRestaurant) {
+  if (leftRestaurant || rightRestaurant) {
     if (side === 'left') {
       removeRestaurant('left')
       renderRestaurant(leftRestaurant, 'left')
@@ -54,14 +54,12 @@ function grabPortion(data, side) {
   } else {
     if (side = 'left') {
       removeRestaurant('left')
-
-      alert(`By process of elimination, ${leftRestaurant.name} is your destination for today!`)
+      // alert(`By process of elimination, ${rightRestaurant.name} is your destination for today!`)
     } else if (side = 'right') {
       removeRestaurant('right')
-      renderRestaurant(rightRestaurant, 'right')
-      alert(`By process of elimination, ${rightRestaurant.name} is your destination for today!`)
+      // alert(`By process of elimination, ${leftRestaurant.name} is your destination for today!`)
     } else {
-      alert('There no other restaurants to review')
+      alert('There are no other restaurants to review')
     }
 
   }
